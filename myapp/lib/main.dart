8c6c4a3646b21ab67acc158a57342c6dc121d7b8
talha925 <t.raheel125@gmail.com>
2022-05-21 02:51:38 +0500

    flutter code al

---
+++ myapp/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screen/Constant_widget/my_theme.dart';
+
+import 'screen/login_screen/loginpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ myapp/lib/screen/Constant_widget/my_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      canvasColor: Colors.white,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Colors.black,
+          onPrimary: Colors.white,
+          secondary: Color.fromARGB(221, 185, 21, 21),
+          onSecondary: Color.fromARGB(255, 255, 200, 0),
+          error: Colors.red,
+          onError: Color.fromARGB(255, 180, 41, 41),
+          background: Colors.black,
+          onBackground: Color.fromARGB(255, 207, 13, 13),
+          surface: Color.fromARGB(96, 204, 18, 18),
+          onSurface: Color.fromARGB(255, 18, 80, 187)));
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+      canvasColor: Colors.black,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Colors.white,
+          onPrimary: Colors.white,
+          secondary: Colors.white,
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.redAccent,
+          background: Colors.black,
+          onBackground: Colors.black,
+          surface: Colors.black38,
+          onSurface: Colors.blueAccent));
+}
